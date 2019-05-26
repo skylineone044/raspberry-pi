d8d7d7fd73533e351b54dac9ff6e08e92c610434
skylineone044 <tokodioli@gmail.com>
2019-05-26 10:08:13 +0200

    Added docstring

--- autohome/relays/switch.py
+++ autohome/relays/switch.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''
+this module interfaces with the rest,
+and calls relay switch with the right argument
+'''
 
 print("\n  --- Relay toggle script started! ---   ")
 #import RPi.GPIO as GPIO
