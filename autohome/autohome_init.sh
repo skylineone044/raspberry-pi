5bf738acab6416feb99cd7afe592b04afc984fc1
skylineone044 <tokodioli@gmail.com>
2019-05-26 08:05:21 +0200

    Replaced echo with printf

--- autohome/autohome_init.sh
+++ autohome/autohome_init.sh
@@ -1,18 +1,18 @@
-echo "\n\n\n"
-echo "--------------------------------"
-echo "--Started Autohome init script--"
-echo "--------------------------------"
+printf "\n\n\n"
+printf "--------------------------------"
+printf "--Started Autohome init script--"
+printf "--------------------------------"
 
-echo "\n-Setting up GPIO Pins..."
+printf "\n-Setting up GPIO Pins..."
 python3 /home/pi/cmd/auto/pinsetup_onboot.py
 
-echo "\n-Setting relay defaults..."
+printf "\n-Setting relay defaults..."
 python3 /home/pi/cmd/auto/relay_init.py
 
-echo "\n-Starting button script..."
+printf "\n-Starting button script..."
 python3 /home/pi/cmd/auto/relay1_button_v2.py
 
-echo "--All done!\n"
+printf "--All done!\n"
 
 #Notes:
 #Place the simlinks of the scripts to /home/pi/cmd/auto for automatic execution
