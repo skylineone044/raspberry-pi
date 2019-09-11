6cab9a3b01f2950189f3a98a0a36f13c82f0e8b1
skylineone044 <tokodioli@gmail.com>
2019-09-11 16:47:53 +0200

    Removed, because it was no longer needed for a long time now

--- autohome/relays/bkup/relay4.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-print("\n  --- Relay 4 toggle script started! ---   ")
-import RPi.GPIO as GPIO
-import relaylist
-
-relaylist.relayswitch(4)
--- autohome/relays/bkup/relay8.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-print("\n  --- Relay 8 toggle script started! ---   ")
-import RPi.GPIO as GPIO
-import relaylist
-
-relaylist.relayswitch(8)
