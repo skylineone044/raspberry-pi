7adbd824510d4b09816240b282f83921f05ca1a5
skylineone044 <skylineone044@gmail.com>
2021-01-18 17:47:40 +0100

    reading and writing config

--- relay_init.py
+++ relay_init.py
@@ -4,21 +4,37 @@ This module sets up the GPIO pins after startup, and sets their valuues
 """
 import os
 import json
+import sys
 
 try:
     import RPi.GPIO
 except (RuntimeError, ModuleNotFoundError):
     print("USING DUMMY GPIO LIB")
-    import fake_rpigpio.utils
+    import fake_rpi
 
-    fake_rpigpio.utils.install()
+    sys.modules["RPi"] = fake_rpi.RPi  # Fake RPi
+    sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO  # Fake GPIO
+    import RPi.GPIO as GPIO
+
 
 print("Starting relay setkup...")
+GPIO.setmode(GPIO.BCM)
 # Set up directory srructure in the ram directory
-
-# with open("state.json", "w") as jsonFile:
-#     data = {"Working_dir": "wdir", "relay_states": [0, 0, 1, 0, 1, 1, 1, 1, 1]}
-#     json.dump(data, jsonFile)
+try:
+    if sys.argv[1] in ("--Write-config", "-Wc"):
+        print("Writing config...")
+        with open("state.json", "w") as jsonFile:
+            data = {
+                "Working_dir": "wdir",
+                "pins": [5, 26, 19, 13, 6, 12, 16, 20, 21, 7],  # pin numbers
+                "pin_directions": [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # 0 in 1 out
+                "pin_states": [0, 0, 1, 0, 1, 1, 1, 1, 1, 1],  # 0 off 1 on
+            }
+            # 0. item is the button, the next 8 are relay pins,
+            # the last in the pins group is the status LED
+            json.dump(data, jsonFile)
+except IndexError:
+    pass
 
 print("Reading config...")
 with open("state.json", "r") as jsonFile:
@@ -29,5 +45,7 @@ print("Creating storage structure...")
 os.system(f"mkdir {data['Working_dir']}")
 with open(f"{data['Working_dir']}/state.json", "w") as statefile:
     json.dump(data, statefile)
+
+print("Setting up pin data directions...")
 
 print("Done!")
