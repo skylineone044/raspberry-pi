4ff0828c4c11bb79492103f99980b9261760116b
skylineone044 <skylineone044@gmail.com>
2021-01-18 16:59:26 +0100

    reading and writing storage structure

--- relay_init.py
+++ relay_init.py
@@ -8,7 +8,26 @@ import json
 try:
     import RPi.GPIO
 except (RuntimeError, ModuleNotFoundError):
+    print("USING DUMMY GPIO LIB")
     import fake_rpigpio.utils
 
     fake_rpigpio.utils.install()
 
+print("Starting relay setkup...")
+# Set up directory srructure in the ram directory
+
+# with open("state.json", "w") as jsonFile:
+#     data = {"Working_dir": "wdir", "relay_states": [0, 0, 1, 0, 1, 1, 1, 1, 1]}
+#     json.dump(data, jsonFile)
+
+print("Reading config...")
+with open("state.json", "r") as jsonFile:
+    data = json.load(jsonFile)
+    print(data)
+
+print("Creating storage structure...")
+os.system(f"mkdir {data['Working_dir']}")
+with open(f"{data['Working_dir']}/state.json", "w") as statefile:
+    json.dump(data, statefile)
+
+print("Done!")
