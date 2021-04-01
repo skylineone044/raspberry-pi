1c7282fed24d4444e111ba239008eae5c2981c63
skylineone044 <skylineone044@gmail.com>
2021-04-01 21:38:33 +0200

    reworked relay init

--- switch.py
+++ switch.py
@@ -2,8 +2,18 @@
 """
 This file does thwe main switchong action
 """
+
+import os
+import json
+import sys
+
 try:
     import RPi.GPIO
 except (RuntimeError, ModuleNotFoundError):
-    import fake_rpigpio.utils
-    fake_rpigpio.utils.install()
+    print("USING DUMMY GPIO LIB")
+    import fake_rpi
+
+    sys.modules["RPi"] = fake_rpi.RPi  # Fake RPi
+    sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO  # Fake GPIO
+    import RPi.GPIO as GPIO
+
