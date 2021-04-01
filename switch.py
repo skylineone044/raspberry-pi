6b10183f4a263f21162d19587fe3986db97c19a7
skylineone044 <skylineone044@gmail.com>
2021-04-01 22:39:01 +0200

    switching works

--- switch.py
+++ switch.py
@@ -17,6 +17,8 @@ except (RuntimeError, ModuleNotFoundError):
     sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO  # Fake GPIO
     import RPi.GPIO as GPIO
 
+WORKING_STATUS_FILE = "wdir/state.json"
+
 try:
     RELAY_TO_BE_SWITCHED = sys.argv[1]
 except IndexError:
@@ -35,8 +37,47 @@ if DESIRED_STATE.upper() not in ("SWITCH", "HIGH", "LOW", "ON", "OFF"):
 
 # Read in current state of the relays
 
-with open("wdir/state.json", "r") as stateFile:
+with open(WORKING_STATUS_FILE, "r") as stateFile:
     data = json.load(stateFile)
     print(data)
 
 
+if DESIRED_STATE == "ON":
+    DESIRED_STATE = "HIGH"
+if DESIRED_STATE == "OFF":
+    DESIRED_STATE = "LOW"
+
+try:
+    if DESIRED_STATE == data["pins"][RELAY_TO_BE_SWITCHED]["state"]:
+        print("Relay is in the desired state, done")
+        exit(0)
+except KeyError:
+    print(
+        f"This relay pin number [{RELAY_TO_BE_SWITCHED}] is not configured to be switched!"
+    )
+    exit(3)
+
+# if we got this far they we need to switch the realy
+
+## switching it
+current_state = data["pins"][RELAY_TO_BE_SWITCHED]["state"]
+if data["pins"][RELAY_TO_BE_SWITCHED]["direction"] == "OUT":
+    if current_state == "HIGH":
+        print(f"Switching pin {RELAY_TO_BE_SWITCHED} to LOW...")
+        GPIO.output(int(RELAY_TO_BE_SWITCHED), GPIO.LOW)
+        data["pins"][RELAY_TO_BE_SWITCHED]["state"] = "LOW"
+    elif current_state == "LOW":
+        print(f"Switching pin {RELAY_TO_BE_SWITCHED} to HIGH...")
+        GPIO.output(int(RELAY_TO_BE_SWITCHED), GPIO.HIGH)
+        data["pins"][RELAY_TO_BE_SWITCHED]["state"] = "HIGH"
+else:
+    print("This pin is configured as an input!")
+    exit(4)
+
+## writing out changed data
+
+with open(WORKING_STATUS_FILE, "w") as stateFile:
+    print("Writing out state...")
+    json.dump(data, stateFile, indent=4)
+
+print("Done")
