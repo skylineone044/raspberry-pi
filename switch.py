f07104a4e2cc9b88307218deeda19e60d6893b36
skylineone044 <skylineone044@gmail.com>
2021-08-29 21:53:32 +0200

    lockfile removal hardening

--- switch.py
+++ switch.py
@@ -29,7 +29,10 @@ LOCKFILE = WORKING_DIRECTORY + "/LOCK"
 
 
 def unlock():
-    os.remove(LOCKFILE)
+    try:
+        os.remove(LOCKFILE)
+    except FileNotFoundError:
+        print("was not locked")
 
 
 def switch(relayPin, toState):
@@ -38,58 +41,61 @@ def switch(relayPin, toState):
         return -1
     open(LOCKFILE, 'a').close()
 
-    toState = toState.upper()
-    if toState not in ("SWITCH", "HIGH", "LOW", "ON", "OFF"):
-        print("Invalid desired state given, aborting...")
-        exit(2)
-
-    # Read in current state of the relays
-
-    with open(WORKING_STATUS_FILE, "r") as stateFile:
-        data = json.load(stateFile)
-        # print(data)
-
-    if toState == "ON":
-        toState = "HIGH"
-    if toState == "OFF":
-        toState = "LOW"
-
     try:
-        if toState == data["pins"][relayPin]["state"]:
-            print("Relay is in the desired state, done")
-            exit(0)
-    except KeyError:
-        print(
-            "This relay pin number [{}] is not configured to be switched!".format(relayPin)
-        )
-        exit(3)
+        toState = toState.upper()
+        if toState not in ("SWITCH", "HIGH", "LOW", "ON", "OFF"):
+            print("Invalid desired state given, aborting...")
+            exit(2)
 
-    # if we got this far they we need to switch the realy
+        # Read in current state of the relays
 
-    # switching it
-    current_state = data["pins"][relayPin]["state"]
-    if data["pins"][relayPin]["direction"] == "OUT":
-        GPIO.setup(int(relayPin), GPIO.OUT)
-        if current_state == "HIGH":
-            print("Switching pin {} to LOW...".format(relayPin))
-            GPIO.output(int(relayPin), GPIO.LOW)
-            data["pins"][relayPin]["state"] = "LOW"
-        elif current_state == "LOW":
-            print("Switching pin {} to HIGH...".format(relayPin))
-            GPIO.output(int(relayPin), GPIO.HIGH)
-            data["pins"][relayPin]["state"] = "HIGH"
-    else:
-        print("This pin is configured as an input!")
-        exit(4)
+        with open(WORKING_STATUS_FILE, "r") as stateFile:
+            data = json.load(stateFile)
+            # print(data)
 
-    # writing out changed data
+        if toState == "ON":
+            toState = "HIGH"
+        if toState == "OFF":
+            toState = "LOW"
 
-    with open(WORKING_STATUS_FILE, "w") as stateFile:
-        print("Writing out state...")
-        json.dump(data, stateFile, indent=4)
+        try:
+            if toState == data["pins"][relayPin]["state"]:
+                print("Relay is in the desired state, done")
+                exit(0)
+        except KeyError:
+            print(
+                "This relay pin number [{}] is not configured to be switched!".format(relayPin)
+            )
+            exit(3)
 
-    time.sleep(0.2)
-    print("Done")
+        # if we got this far they we need to switch the realy
+
+        # switching it
+        current_state = data["pins"][relayPin]["state"]
+        if data["pins"][relayPin]["direction"] == "OUT":
+            GPIO.setup(int(relayPin), GPIO.OUT)
+            if current_state == "HIGH":
+                print("Switching pin {} to LOW...".format(relayPin))
+                GPIO.output(int(relayPin), GPIO.LOW)
+                data["pins"][relayPin]["state"] = "LOW"
+            elif current_state == "LOW":
+                print("Switching pin {} to HIGH...".format(relayPin))
+                GPIO.output(int(relayPin), GPIO.HIGH)
+                data["pins"][relayPin]["state"] = "HIGH"
+        else:
+            print("This pin is configured as an input!")
+            exit(4)
+
+        # writing out changed data
+
+        with open(WORKING_STATUS_FILE, "w") as stateFile:
+            print("Writing out state...")
+            json.dump(data, stateFile, indent=4)
+
+    finally:
+        time.sleep(0.2)
+        print("Done")
+        unlock()
 
 
 def relayLookup(relayPin):
